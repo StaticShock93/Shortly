b9b717e592da6022008146b01c3aa87f07203a37
Chacta  Brice <chactaib@gmail.com>
2025-04-13 20:10:10 -0700

    testing

--- Shortly_Frontend/vite.config.ts
+++ Shortly_Frontend/vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/Shortly_Frontend/',
   plugins: [react(), tailwindcss()],
 })
