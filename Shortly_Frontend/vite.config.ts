9f4d0c12420d1e553f7ff400eabdd47ee19e7c1b
Chacta  Brice <chactaib@gmail.com>
2025-06-05 17:55:02 -0700

    fixed defineConfig

--- Shortly_Frontend/vite.config.ts
+++ Shortly_Frontend/vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Shortly_Frontend/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
